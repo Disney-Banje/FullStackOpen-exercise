2486bec381e2628674cdc1f75f6f20ad5d41dd06
Disney <disneyconstant996@gmail.com>
2024-06-26 17:38:22 +0300

    Finished step 2 of course information exercises. On refactoring the Content component

--- part1/courseinfo/src/Content.jsx
+++ part1/courseinfo/src/Content.jsx
@@ -1,8 +1,18 @@
+import Part from "./Part";
+
 const Content = (props) => {
     return (
-        <p>
-            {props.part} {props.exercises}
-        </p>
+        <div>
+            <Part 
+            name={props.part1[0]}
+            number={props.part1[1]} />
+            <Part 
+            name={props.part2[0]}
+            number={props.part2[1]} />
+            <Part 
+            name={props.part3[0]}
+            number={props.part3[1]} />
+        </div>
     )
 }
 
